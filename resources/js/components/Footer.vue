e7ab2913e04bb18e1d6c7b53ada7f94e68450dd1
Westpoint Team <team@westpoint.com>
2026-02-05 12:27:21 +0100

    Copyright-Jahr auf 2025-2026 geÃ¤ndert

--- resources/js/components/Footer.vue
+++ resources/js/components/Footer.vue
@@ -2,7 +2,7 @@
     <footer class="">
         <div class="mx-auto max-w-[1420px] px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
             <div>
-                <p class="mt-8 text-start text-sm/6 text-gray-400 md:order-1 md:mt-0">&copy; 2020-2026 Westpoint, Alle Rechte vorbehalten.</p>
+                <p class="mt-8 text-start text-sm/6 text-gray-400 md:order-1 md:mt-0">&copy; 2025-2026 Westpoint, Alle Rechte vorbehalten.</p>
                 <p class="mt-8 text-start text-sm/6 text-gray-400 md:order-1 md:mt-0">Westpoint hat keine offizielle Verbindung zu Rockstar Games, Take-Two Interactive Software.</p>
             </div>
         </div>
