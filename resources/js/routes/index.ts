f9352f309d22bce093e9d2232441ff4e01279890
Westpoint Team <team@westpoint.com>
2026-02-14 10:41:58 +0100

    Fix: Export logout aus routes/index.ts - Alias fÃ¼r destroy aus AuthenticatedSessionController

--- resources/js/routes/index.ts
+++ resources/js/routes/index.ts
@@ -1,2 +1,5 @@
 import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../wayfinder'
-import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../wayfinder'
+import { destroy } from '@/actions/Laravel/Fortify/Http/Controllers/AuthenticatedSessionController'
+
+// Export logout als Alias für destroy (funktioniert sowohl als Funktion als auch als Objekt mit .url())
+export const logout = destroy
